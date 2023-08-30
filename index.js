438a76ea3248d8b036661f0e71da9935130f8db4
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-08-30 21:15:26 +0600

    modal added

---
+++ index.js
@@ -0,0 +1,105 @@
+const handleCategory = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories"
+  );
+  const data = await response.json();
+  //   console.log(data.data.news_category);
+  const newsCategory = data.data.news_category;
+  const tabContainer = document.getElementById("tab-container");
+  newsCategory.slice(0, 3).forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<a onclick="handleLoadNews('${category.category_id}')" class="tab">${category.category_name}</a>`;
+    tabContainer.appendChild(div);
+  });
+};
+handleCategory();
+
+const handleLoadNews = async (category_id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${category_id}`
+  );
+  const data = await response.json();
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = ``;
+  console.log(data.data);
+  data.data?.forEach((news) => {
+    const div = document.createElement("div");
+    div.innerHTML = ` <div class="card w-96 bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src=${news?.image_url}
+        alt="Shoes"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        ${news.title.slice(0, 40)}
+        <div class="badge badge-secondary p-5"> ${news.rating?.badge}</div>
+      </h2>
+      <p>
+      ${news.details.slice(0, 50)}
+      </p>
+      <div class="card-footer flex justify-between mt-8">
+        <div class="flex">
+          <div>
+            <div class="avatar online">
+              <div class="w-14 rounded-full">
+                <img
+                  src= ${news.author?.img}
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <h6>${news.author?.name}</h6>
+            <small>${news.author?.published_date}</small>
+          </div>
+        </div>
+        <div class="card-detaild-btn">
+          <button
+            class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900" onclick="handleModal('${
+              news?._id
+            }')"
+          >
+            Details
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+    cardContainer.appendChild(div);
+  });
+};
+handleLoadNews("01");
+
+const handleModal = async (newsId) => {
+  console.log(newsId);
+
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/news/${newsId}`
+  );
+  const data = await response.json();
+  console.log(data);
+
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.innerHTML = `
+      
+  <dialog id="my_modal_1" class="modal">
+    <form method="dialog" class="modal-box">
+      <h3 class="font-bold text-lg">Hello!</h3>
+      <p class="py-4">Press ESC key or click the button below to close</p>
+      <div class="modal-action">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </div>
+    </form>
+  </dialog>
+      `;
+
+  const modal = document.getElementById("my_modal_1");
+  modal.showModal();
+};
+
+const handleLoader = (status) => {
+  document.getElementById("loader").style.display = status;
+};
